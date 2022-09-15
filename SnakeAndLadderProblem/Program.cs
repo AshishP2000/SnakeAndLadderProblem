7a5bad438d9f89a9022484fd432c39429ded12cf
Ashish <ashish2000patil@gmail.com>
2022-09-15 22:32:27 +0530

    [Ashish]Added: Rolls Die to get Number between 1 to 6

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -11,8 +11,15 @@ namespace SnakeAndLadderProblem
     {
         static void Main(string[] args)
         {
-            //UC-1 Snake and Ladder game played with single player at start position 0
-            int Start_Position = 0;
+            //UC-2 Player Rolls a die to get number between 1 to 6
+            int Start_Position = 0,r=0;
+            Random random = new Random();//Random function to generate random number
+            r = random.Next(1,7);//Giving range from 1 to 6
+            Console.WriteLine("Rolled Dice");
+            Console.WriteLine("Number got: {0}",r);
+
+
+
             Console.ReadLine();
         }
     }
