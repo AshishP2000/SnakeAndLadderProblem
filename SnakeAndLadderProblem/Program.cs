d53f8e0d7422d107c9f848c26e2d9ec6d8c532eb
Ashish <ashish2000patil@gmail.com>
2022-09-15 22:26:00 +0530

    [Ashish]Added: Starting Postion at 0

---
+++ SnakeAndLadderProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1 Snake and Ladder game played with single player at start position 0
+            int Start_Position = 0;
+            Console.ReadLine();
+        }
+    }
+}
