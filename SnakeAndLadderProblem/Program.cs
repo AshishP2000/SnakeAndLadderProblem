612b1a9be834310d908dee1950d287b5717ec6aa
Ashish <ashish2000patil@gmail.com>
2022-09-18 18:10:01 +0530

    [Ashish]Added: Repeat till player reaches winning position 100 UC-4

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -11,34 +11,41 @@ namespace SnakeAndLadderProblem
     {
         static void Main(string[] args)
         {
-            //UC-3 Player then checks for a option there are no play ,ladder or snake
-            int Start_Position = 0,r=0,choice=0;
+            //UC-4 Repeat till the player reaches the winning position 100
+            int Start_Position = 0,r=0,choice=0,count=0;
             const int NOPLAY = 0;
             const int LADDER = 1;
             const int SNAKE = 2;
             Random random = new Random();//Random function to generate random number
-            r = random.Next(1,7);//Giving range from 1 to 6
-            Console.WriteLine("Rolled Dice");
-            Console.WriteLine("Number got: {0}",r);
-
-            choice = random.Next(0,3);
-            Console.WriteLine("Player Choice: {0}", choice);
-
-            switch (choice)
+            while (Start_Position < 100)
             {
-                case NOPLAY:
-                    Console.WriteLine("Position: {0}",Start_Position);
-                    break;
-                case LADDER:
-                    Start_Position = Start_Position+r;
-                    Console.WriteLine("Position: {0}", Start_Position);
-                    break;
-                case SNAKE:
-                    Start_Position = Start_Position - r;
-                    Console.WriteLine("Position: {0}", Start_Position);
-                    break;
-            }
+                r = random.Next(1, 7);//Giving range from 1 to 6
+                //Console.WriteLine("Rolled Dice");
+                Console.WriteLine("Number got: {0}", r);
+                choice = random.Next(0, 3);
+                Console.WriteLine("Player Choice: {0}", choice);
+                switch (choice)
+                {
+                    case NOPLAY:
+                        Console.WriteLine("Position: {0}", Start_Position);
+                        break;
+                    case LADDER:
+                        Start_Position = Start_Position + r;
+                        Console.WriteLine("Position: {0}", Start_Position);
+                        break;
+                    case SNAKE:
+                        Start_Position = Start_Position - r;
+                        if (Start_Position < 0)
+                        {
+                            Start_Position = 0;
+                        }
+                        Console.WriteLine("Position: {0}", Start_Position);
+                        break;
+                }
 
+                count++;
+
+            }
             Console.ReadLine();
         }
     }
