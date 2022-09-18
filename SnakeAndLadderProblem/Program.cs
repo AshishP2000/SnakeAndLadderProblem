b2529ab19cd49edb72cf9c1ae0850325da99cabb
Ashish <ashish2000patil@gmail.com>
2022-09-18 09:42:23 +0530

    [Ashish]Added: Checks for Option UC-3

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -11,14 +11,33 @@ namespace SnakeAndLadderProblem
     {
         static void Main(string[] args)
         {
-            //UC-2 Player Rolls a die to get number between 1 to 6
-            int Start_Position = 0,r=0;
+            //UC-3 Player then checks for a option there are no play ,ladder or snake
+            int Start_Position = 0,r=0,choice=0;
+            const int NOPLAY = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
             Random random = new Random();//Random function to generate random number
             r = random.Next(1,7);//Giving range from 1 to 6
             Console.WriteLine("Rolled Dice");
             Console.WriteLine("Number got: {0}",r);
 
+            choice = random.Next(0,3);
+            Console.WriteLine("Player Choice: {0}", choice);
 
+            switch (choice)
+            {
+                case NOPLAY:
+                    Console.WriteLine("Position: {0}",Start_Position);
+                    break;
+                case LADDER:
+                    Start_Position = Start_Position+r;
+                    Console.WriteLine("Position: {0}", Start_Position);
+                    break;
+                case SNAKE:
+                    Start_Position = Start_Position - r;
+                    Console.WriteLine("Position: {0}", Start_Position);
+                    break;
+            }
 
             Console.ReadLine();
         }
